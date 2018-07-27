087608935c7a83d6cd303cb8858c8a3b97999c20
pdementevna@gmail.com <Vancouver1984>
2018-07-27 16:01:15 +0300

    Открытка генерится со всеми записями, шрифт как Аня просила

--- indexJIMP.js
+++ indexJIMP.js
@@ -1,65 +1,67 @@
-// let Jimp = require("jimp");
+let Jimp = require("jimp");
 
-// адресат - 12pt/14pt bold
-// обращение - 21pt/25pt
-// текст - 16pt/22pt
-// текст наш - 12pt/14pt
-
-function textOnPostcard (filenameIN, personName, textPostcard) {
+function textOnPostcard (filenameIN, nameFrom, nameTo, textPostcard) {
   let filenameOUT = 'image/postcard/postcard.jpg';
   let loadedImage;
-  let footerText = 'Вы держите в руках самый Большой и Настоязий подарок: теплу. и искреннюю благодарнрсть' +
+  let footerText = 'Вы держите в руках самый Большой и Настоящий подарок: теплу. и искреннюю благодарность' +
     ' от маленького пациента, который ведет борьбу с тяжелым недугом. Именно ему Вы с нашей помощью передали огромную ' +
     'ценность - шанс на выздоровление. Фонд "Подари жизнь" помогает детям с тяжелыми онкогематологическими заболеваниями. И ' +
     'благодаря Вашей поддержке мы имеем возможность помогать детям лечиться, а врачам лечить. Спасибо Вам!';
 
-  Jimp.read(filenameIN)
+  Jimp.read(filenameOUT)
     .then((image) => {
       loadedImage = image;
-      return Jimp.loadFont('font/font/font.fnt');
+      return Jimp.loadFont('font/fontNameFrom/font.fnt');
     })
     .then((font) => {
-      loadedImage.print(font, 306, 588, personName)
-        .print(font, 142, 695, textPostcard, 705)
-        .print(font, 96, 1137, footerText, 609)
+      loadedImage.print(font, 773, 487, nameFrom, 232)
         .write(filenameOUT);
     })
     .catch((err) => {
       console.error(err);
     });
 
-  // Jimp.read(filenameOUT)
-  //   .then((image) => {
-  //     loadedImage = image;
-  //     return Jimp.loadFont('font/font/font.fnt');
-  //   })
-  //   .then((font) => {
-  //     loadedImage.print(font, 142, 695, textPostcard, 705)
-  //       .write(filenameOUT);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
-  //
-  // Jimp.read(filenameOUT)
-  //   .then((image) => {
-  //     loadedImage = image;
-  //     return Jimp.loadFont('font/font/font.fnt');
-  //   })
-  //   .then((font) => {
-  //     loadedImage.print(font, 96, 1137, footerText, 609)
-  //       .write(filenameOUT);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
+  Jimp.read(filenameOUT)
+    .then((image) => {
+      loadedImage = image;
+      return Jimp.loadFont('font/fontNameTo/font.fnt');
+    })
+    .then((font) => {
+      loadedImage.print(font, 444, 588, nameTo)
+        .write(filenameOUT);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
+  Jimp.read(filenameOUT)
+    .then((image) => {
+      loadedImage = image;
+      return Jimp.loadFont('font/fontText/font.fnt');
+    })
+    .then((font) => {
+      loadedImage.print(font, 142, 695, textPostcard, 705)
+        .write(filenameOUT);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  Jimp.read(filenameOUT)
+    .then((image) => {
+      loadedImage = image;
+      return Jimp.loadFont('font/fontPZ/font.fnt');
+    })
+    .then((font) => {
+      loadedImage.print(font, 96, 1137, footerText, 609)
+        .write(filenameOUT);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
-let textPostcard = 'Вы держите в руках самый Большой и Настоязий подарок: теплу. и искреннюю благодарнрсть' +
-  ' от маленького пациента, который ведет борьбу с тяжелым недугом. Именно ему Вы с нашей помощью передали огромную ' +
-  'ценность - шанс на выздоровление. Фонд "Подари жизнь" помогает детям с тяжелыми онкогематологическими заболеваниями. И ' +
-  'благодаря Вашей поддержке мы имеем возможность помогать детям лечиться, а врачам лечить. Спасибо Вам!';
+let textPostcard = 'Поздравляю!';
+let nameTo = 'Иван Иванович!';
+let nameFrom = 'Полина';
 
-let name = 'Дорогой Иван Иванович!';
-
-textOnPostcard('image/postcard1.jpg', name, textPostcard);
+textOnPostcard('image/postcard1.jpg', nameFrom, nameTo, textPostcard);
